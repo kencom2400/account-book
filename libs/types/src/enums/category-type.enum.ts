7d16cc7f07da0803db61ef39185576509365ca44
kencom2400 <kencom@mac.com>
2025-11-24 22:13:09 +0900

    feat: CategoryType Enum追加 (#290)

---
+++ libs/types/src/enums/category-type.enum.ts
@@ -0,0 +1,16 @@
+/**
+ * カテゴリタイプ
+ * 取引の主要な分類を表すEnum
+ */
+export enum CategoryType {
+  /** 収入 */
+  INCOME = 'INCOME',
+  /** 支出 */
+  EXPENSE = 'EXPENSE',
+  /** 振替 */
+  TRANSFER = 'TRANSFER',
+  /** 返済 */
+  REPAYMENT = 'REPAYMENT',
+  /** 投資 */
+  INVESTMENT = 'INVESTMENT',
+}
